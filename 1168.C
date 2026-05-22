fb4aeb377e85da4ab6bc16dc12eb344be4af5919
AlexandreOliveiradaSilva <alexandre.ramalho@unifesp.br>
2026-05-22 09:21:09 -0300

    Create 1168.C

---
+++ 1168.C
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, total;
+    char numero[101];
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+
+        scanf("%s", numero);
+
+        total = 0;
+
+        for(j = 0; numero[j] != '\0'; j++) {
+
+            switch(numero[j]) {
+
+                case '1':
+                    total += 2;
+                    break;
+
+                case '7':
+                    total += 3;
+                    break;
+
+                case '4':
+                    total += 4;
+                    break;
+
+                case '2':
+                case '3':
+                case '5':
+                    total += 5;
+                    break;
+
+                case '0':
+                case '6':
+                case '9':
+                    total += 6;
+                    break;
+
+                case '8':
+                    total += 7;
+                    break;
+            }
+        }
+
+        printf("%d leds\n", total);
+    }
+
+    return 0;
+}
