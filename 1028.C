fefd07ef1a52a0cc044e489bd6bbc3c109242e99
AlexandreOliveiradaSilva <alexandre.ramalho@unifesp.br>
2026-05-26 16:45:01 -0300

    Create 1028.C

---
+++ 1028.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int mdc(int a, int b) {
+    
+    if (b == 0) {
+      return a;
+    }
+    
+    return mdc(b, a % b);
+}
+
+int main() {
+    
+    int N;
+    int F1, F2;
+    
+    scanf("%d", &N);
+    
+    for( int i = 0; i < N; i++) {
+        scanf("%d %d", &F1, &F2);
+        
+        printf("%d\n", mdc(F1, F2));
+    }
+    return 0;
+}
