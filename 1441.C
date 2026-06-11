fb71a916ac8ddf58680fc434137937e99b133eac
AlexandreOliveiradaSilva <alexandre.ramalho@unifesp.br>
2026-06-11 09:52:55 -0300

    Create 1441.C

---
+++ 1441.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+long long granizo(long long n, long long maior) {
+    if (n > maior)
+        maior = n;
+
+    if (n == 1)
+        return maior;
+
+    if (n % 2 == 0)
+        return granizo(n / 2, maior);
+
+    return granizo(3 * n + 1, maior);
+}
+
+int main() {
+    long long h;
+
+    while (scanf("%lld", &h) && h) {
+        printf("%lld\n", granizo(h, h));
+    }
+
+    return 0;
+}
