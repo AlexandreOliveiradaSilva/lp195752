7260d13d628aed2ba4fb5fd4dacd8bbdbeaad8f6
AlexandreOliveiradaSilva <alexandre.ramalho@unifesp.br>
2026-06-11 09:51:00 -0300

    Create 1234.C

---
+++ 1234.C
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char linha[100];
+
+    while (fgets(linha, sizeof(linha), stdin) != NULL) {
+        size_t len = strlen(linha);
+        if (len > 0 && linha[len - 1] == '\n') {
+            linha[len - 1] = '\0';
+        }
+
+        int proximo_eh_maiusculo = 1;
+
+        for (int i = 0; linha[i] != '\0'; i++) {
+            if (isalpha(linha[i])) {
+                if (proximo_eh_maiusculo) {
+                    linha[i] = toupper(linha[i]);
+                    proximo_eh_maiusculo = 0;
+                } else {
+                    linha[i] = tolower(linha[i]);
+                    proximo_eh_maiusculo = 1;
+                }
+            }
+        }
+
+        printf("%s\n", linha);
+    }
+
+    return 0;
+}
