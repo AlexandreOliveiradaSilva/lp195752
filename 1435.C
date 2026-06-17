9f64575d32e7f7527f9b6515b678eb47f235e731
AlexandreOliveiradaSilva <alexandre.ramalho@unifesp.br>
2026-06-17 14:54:58 -0300

    Create 1435.C

---
+++ 1435.C
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    do
+    {
+        scanf("%d", &n);
+
+        if (n == 0)
+            break;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int menor = i;
+
+                if (j < menor)
+                    menor = j;
+
+                if (n - 1 - i < menor)
+                    menor = n - 1 - i;
+
+                if (n - 1 - j < menor)
+                    menor = n - 1 - j;
+
+                if (j == 0)
+                    printf("%3d", menor + 1);
+                else
+                    printf(" %3d", menor + 1);
+            }
+
+            printf("\n");
+        }
+
+        printf("\n");
+
+    } while (1);
+
+    return 0;
+}
