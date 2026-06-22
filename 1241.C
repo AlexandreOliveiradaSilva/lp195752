19bad6c2ba224487b2295ed123f494379e945c97
AlexandreOliveiradaSilva <alexandre.ramalho@unifesp.br>
2026-06-22 11:40:55 -0300

    Update 1241.C

--- 1241.C
+++ 1241.C
@@ -1,3 +1,10 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Alexandre OLiveira da Silva Ramalho
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 #include <string.h>
 
