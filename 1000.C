3a7e01179d8ff3249a2b1986e687126ec7a1f799
AlexandreOliveiradaSilva <alexandre.ramalho@unifesp.br>
2026-06-22 11:33:14 -0300

    Update 1000.C

--- 1000.C
+++ 1000.C
@@ -1,3 +1,11 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Alexandre OLiveira da Silva Ramalho
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+-------------------------------------------------------------------------- */
+
+
 #include <stdio.h>
 
 int main() {
